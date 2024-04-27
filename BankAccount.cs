fc2ebf8360a2795c717036fa2227328218f43f88
Renzo1810 <117410730+Renzo1810@users.noreply.github.com>
2024-04-27 12:39:39 -0500

    Add files via upload

---
+++ BankAccount.cs
@@ -0,0 +1,10 @@
+namespace Parcial_Progra.Models;
+
+public class CuentaBancaria
+{
+    public int Id { get; set; }
+    public string? NombreTitular { get; set; }
+    public string? TipoCuenta { get; set; }
+    public decimal SaldoInicial { get; set; }
+    public string? Email { get; set; }
+}
---
+++ BankAccountController.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Parcial_Progra.Controllers
+{
+    [Route("[controller]")]
+    public class BankAccountController : Controller
+    {
+        private readonly ILogger<BankAccountController> _logger;
+
+        public BankAccountController(ILogger<BankAccountController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View("Error!");
+        }
+    }
+}
